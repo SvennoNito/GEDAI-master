7cbf8c46975ce97a206bcafd901f9c551161ac28
NeuroTuning Lab <tomas.ros@unige.ch>
2026-04-29 15:20:50 +0200

    Use sensai_norm_obj for neg_score
    Replace the previous raw_quality_obj output with sensai_norm_obj from SENSAI_basic and negate it to compute neg_score. This updates the output index to match the revised SENSAI_basic return order and ensures the normalized sensitivity metric is used when scoring reconstructed EEG data.

--- GEDAI.m
+++ GEDAI.m
@@ -900,7 +900,7 @@ end
         % Reconstruct and score
         EEGclean_obj_data = cl_wv_obj;
         EEGartifacts_obj_data = EEGavRef.data(:, 1:size(EEGclean_obj_data, 2)) - EEGclean_obj_data;
-        [~, ~, ~, ~, ~, ~, ~, raw_quality_obj, ~] = SENSAI_basic(double(EEGclean_obj_data), double(EEGartifacts_obj_data), srate, broadband_epoch_size, refCOV, 1, signal_type);
-        neg_score = -raw_quality_obj;
+        [~, ~, ~, ~, ~, ~, sensai_norm_obj, ~, ~] = SENSAI_basic(double(EEGclean_obj_data), double(EEGartifacts_obj_data), srate, broadband_epoch_size, refCOV, 1, signal_type);
+        neg_score = -sensai_norm_obj;
     end
 end
